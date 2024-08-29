f706a192d7dd9d8b625719f0c7e19156856f4750
Yegeta <yttaye100@gmail.com>
2024-08-29 13:30:23 +0300

    Handle Submit Issue errors

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,14 +1,57 @@
-import React from 'react'
-import {TextField, TextArea, Button} from '@radix-ui/themes'
+"use client";
 
-const NewIssuePage = () => {
-  return (
-    <div className='max-w-xl space-y-3'>
-      <TextField.Root placeholder='Title' />
-      <TextArea placeholder="Description" />
-      <Button>Add New Issue</Button>
-    </div>
-  )
+import { TextField, TextArea, Button, Callout } from "@radix-ui/themes";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+interface IssueForm {
+  title: string;
+  description: string;
 }
 
-export default NewIssuePage
+const NewIssuePage = () => {
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const router = useRouter();
+  const [error, setError] = useState("");
+
+  return (
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>
+            {error}
+          </Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className=" space-y-3"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/issues", data);
+
+            router.push("/issues");
+          } catch (error) {
+            setError("An unexpected error occured");
+            console.log(error);
+          }
+        })}
+      >
+        <TextField.Root placeholder="Title" {...register("title")} />
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
+        />
+        <Button>Add New Issue</Button>
+      </form>
+    </div>
+  );
+};
+
+export default NewIssuePage;
